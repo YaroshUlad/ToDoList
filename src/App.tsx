c378314817be1fa1cba5fba0aa94c885c4ae3181
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-04-12 09:20:24 +0300

    create universal button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {v1} from "uuid";
+import {Button} from "./components/Button";
 
 export type ObjectsInTaskArrayType = {
     id: string
@@ -18,6 +19,7 @@ const App = () => {
     return (
         <div>
             App
+            <Button buttonTitle={'+'} callBack={() => console.log('aa')}/>
         </div>
     )
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
+type ButtonPropsType = {
+    buttonTitle: string
+    callBack: () => void
+}
+
+export const Button = (props: ButtonPropsType) => {
+    const onClickHandler = () => {
+        props.callBack()
+    }
+    return (
+        <span>
+            <button onClick={onClickHandler}>{props.buttonTitle}</button>
+        </span>
+    )
+}
+
 
 /*import React from "react";
 
