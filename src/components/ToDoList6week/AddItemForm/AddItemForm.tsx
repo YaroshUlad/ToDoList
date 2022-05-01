81e3d3f70616bece6c1f0fe892270377d93ae001
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-05-01 19:01:25 +0300

    development AddItemForm component

--- src/components/ToDoList6week/AddItemForm/AddItemForm.tsx
+++ src/components/ToDoList6week/AddItemForm/AddItemForm.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 
 export const AddItemForm = () => {
-    
+    const [value, setValue] = useState<string>('')
+    const [error, setError] = useState<string>('')
+
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setError('')
+        setValue(e.currentTarget.value)
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+            onClickHandler()
+        }
+    }
+    const onClickHandler = () => {
+        if (value.trim()) {
+            console.log(value.trim()) // send value with callBack
+            setValue('')
+        } else {
+            setError('Field is required')
+        }
+    }
 
     return (
         <div>
-            Add item
-            <input type="text"/>
-            <button>+</button>
+            <div>
+                <input value={value} onKeyPress={onKeyPressHandler} onChange={onChangeHandler} type="text"/>
+                <button onClick={onClickHandler}>+</button>
+            </div>
+            {error ? <span>{error}</span> : <></>}
         </div>
     );
 }
