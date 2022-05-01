8928c58abd990d2ad06bd906d329be7039b0abb4
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-05-01 19:30:19 +0300

    creating MutableSpan component and MappingTasks component

--- src/components/ToDoList6week/ToDoList/ToDoList.tsx
+++ src/components/ToDoList6week/ToDoList/ToDoList.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {AddItemForm} from "../AddItemForm/AddItemForm";
+import {v1} from "uuid";
 
 export type ToDoListPropsType = {
     addNewTask: (newItemTitle: string) => void
 }
 
 export const ToDoList = () => {
+    const Arr = [
+        {id: v1(), title: "HTML&CSS", isDone: true},
+        {id: v1(), title: "JS", isDone: true},
+        {id: v1(), title: "ReactJS", isDone: false},
+        {id: v1(), title: "Rest API", isDone: false}
+    ]
     const addNewTask = (newItemTitle: string) => {
 
     }
@@ -15,6 +22,35 @@ export const ToDoList = () => {
             TodoList
             <h3>What To Learn</h3>
             <AddItemForm callBack={addNewTask}/>
+            <MappingTasks data={Arr}/>
         </div>
     );
+}
+
+type TasksObjectType = {
+    id: string
+    title: string
+    isDone: boolean
+}
+
+type MappingTasksPropsType = {
+    data: Array<TasksObjectType>
+}
+
+const MappingTasks = (props: MappingTasksPropsType) => {
+    return (
+        <ol>
+            {props.data.map((el, index) => {
+                const checkBoxOnChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+                    console.log(e.currentTarget.checked)
+                }
+                return (
+                    <li key={el.id} value={index + 1}>
+                        <input type="checkbox" checked={el.isDone} onChange={checkBoxOnChangeHandler}/>
+                        <span>{el.title}</span>
+                    </li>
+                )
+            })}
+        </ol>
+    )
 }
